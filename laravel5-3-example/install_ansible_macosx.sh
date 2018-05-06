0e99e197e1ef158ba8232ed90b432cd4cde19c86
dungmanh88 <luke.nothingness@gmail.com>
2018-05-06 11:23:35 +0700

    Delele laravel5-3-example and update remove php common if php version is changed

--- laravel5-3-example/install_ansible_macosx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Install pip"
-easy_install pip
-echo "Install ansible"
-pip install ansible
--- laravel5-3-example/install_vagrant_plugin_rsync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Go to folder has Vagrantfile"
-vagrant plugin install vagrant-rsync-back && \
-vagrant plugin install vagrant-gatling-rsync
